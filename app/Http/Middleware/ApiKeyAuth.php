92d16b48516b3ef62f360fd7b86eb9e2366b401a
Mohammad Reza Payami <mr633387@gmail.com>
2025-10-22 03:22:06 +0330

    Add ApiKeyAuth middleware for authentication

---
+++ app/Http/Middleware/ApiKeyAuth.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiKeyAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $apiKey = $request->header('X-API-KEY');
+        if ($apiKey !== 'base64:FDiTrLSYPbNkEHKvbhqeGqv7WK0jq6DgHOy7zUhECoI=') { // در تولید، این رو از env بگیرید
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,22 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\ApiKeyAuth;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(ApiKeyAuth::class);
+
+        // Middleware مخصوص route
+        $middleware->alias([
+            'check.age' => ApiKeyAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
